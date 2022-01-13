11c332120a5851526bba5a46f61edf9656418799
CLS <clindforsSejsing@gmail.com>
2022-01-13 10:37:57 +0100

    rewriting file, using express

--- express-exampel/index.js
+++ express-exampel/index.js
@@ -1,23 +1,30 @@
-import express from "express";
+import express from 'express';
 import fs from "fs/promises";
 
 const app = express();
 
 app.get('/', async (request, response) => {
-    const fileBuf = await fs.readFile('index.html');
-    // response.header('Content-type', 'text/html');
+    const fileBuf = await fs.readFile('./files/index.html');
     response.type('html');
+    response.send(fileBuf);
+});
+
+//find files even in other maps
+app.get('/*', async (request, response) => {
+    const fileName = request.path;
+    const fileBuf = await fs.readFile(`./files/${fileName}`);
+    const type = fileName.split('.')[1];
+    response.type(type);
     response.send(fileBuf);
 // response.send("jajamensan");
 });
-
-
+// app.use(express.static)
 //test-exampel
 // app.get('/hello', (request, response) =>{
 //     response.send('hello hello hello');
 // });
 
-// //rout makes it possible on all methods
+// // //rout makes it possible on all methods
 // app.use('/goodbye', (request, response) => {
 // response.send("what are you waiting for?")
 // });
